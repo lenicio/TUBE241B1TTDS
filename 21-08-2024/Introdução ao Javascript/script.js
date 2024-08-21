8b769de5af409de191a29da620875e702aaf4c67
Lenício Jr <lenicio.junior@hotmail.com>
2024-08-21 17:27:49 -0300

    Commit final de aula

--- 21-08-2024/Introdução ao Javascript/script.js
+++ 21-08-2024/Introdução ao Javascript/script.js
@@ -3,6 +3,13 @@ document.getElementById('btn-enviar')
   e.preventDefault(); // Impede a pag de recarregar ao enviar o formulário
 
   let nome = document.getElementById('input-nome').value;
+  let idade = document.getElementById('input-idade').value;
+
+
+  if(idade < 0) {
+    alert("Idade inválida!");
+  }
+
 
   document.getElementById('nome').innerText = `O nome digitado foi: ${nome}`;
   
